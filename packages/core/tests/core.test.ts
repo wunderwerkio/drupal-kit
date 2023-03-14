9f1d55bf5b0b22c9c612d184f409221b4079d9b2
chfoidl <12481286+chfoidl@users.noreply.github.com>
2023-03-14 14:21:48 +0100

    build: update turborepo config

--- packages/core/tests/core.test.ts
+++ packages/core/tests/core.test.ts
@@ -2,7 +2,7 @@ import { Drupalkit } from "../src";
 
 describe("core", () => {
   it("should instanciate", () => {
-    const drupalkit = new Drupalkit({
+    new Drupalkit({
       baseUrl: "https://drupal-headless-boilerplate.ddev.site",
     });
   });
