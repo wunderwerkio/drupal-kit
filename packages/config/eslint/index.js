8e6d2737a13b47d2362430ee1a15ca970721e1ca
Christian Foidl <12481286+chfoidl@users.noreply.github.com>
2023-05-17 16:24:07 +0200

    build(eslint): fix rules after update

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -3,5 +3,10 @@ module.exports = {
   extends: ["@wunderwerk/eslint-config/typescript"],
   rules: {
     "new-cap": "off",
+    "jsdoc/tag-lines": [
+      "error" | "warn",
+      "never",
+      { tags: { param: { lines: "always" } } },
+    ],
   },
 };
