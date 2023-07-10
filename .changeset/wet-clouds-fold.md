ce29702c155d822676ec314419e419b0c2f61083
Christian Foidl <12481286+chfoidl@users.noreply.github.com>
2023-07-10 15:46:48 +0200

    feat(jsonapi): add method to fetch menu_items for a menu #47

---
+++ .changeset/wet-clouds-fold.md
@@ -0,0 +1,5 @@
+---
+"@drupal-kit/jsonapi": minor
+---
+
+Add support for JSON:API /menu_items/{menu} endpoint
---
+++ packages/jsonapi/tests/menu.test.ts
@@ -0,0 +1,77 @@
+import test from "ava";
+import { DrupalJsonApiParams } from "drupal-jsonapi-params";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { Drupalkit } from "@drupal-kit/core";
+
+import { DrupalkitJsonApi, DrupalkitJsonApiError } from "../src/index.js";
+import JsonApiMenuItems from "./fixtures/jsonapi_menu_items.json" assert { type: "json" };
+import JsonApiMenuItemsError from "./fixtures/jsonapi_menu_items_error.json" assert { type: "json" };
+
+const BASE_URL = "https://my-drupal.com";
+
+type test = string;
+
+const createDrupalkit = ({ baseUrl = BASE_URL }: { baseUrl?: string } = {}) => {
+  const EnhancedDrupalkit = Drupalkit.plugin(DrupalkitJsonApi);
+
+  return new EnhancedDrupalkit({
+    baseUrl,
+    locale: "de",
+    defaultLocale: "de",
+  });
+};
+
+const server = setupServer();
+
+test.before(() => {
+  server.listen();
+});
+
+test.afterEach(() => {
+  server.resetHandlers();
+});
+
+test.after(() => {
+  server.close();
+});
+
+test("Get menu items", async (t) => {
+  const drupalkit = createDrupalkit();
+
+  server.use(
+    rest.get("*/jsonapi/menu_items/my_menu", (_req, res, ctx) =>
+      res(
+        ctx.set("Content-Type", "application/vnd.api+json"),
+        ctx.json(JsonApiMenuItems),
+      ),
+    ),
+  );
+
+  const result = await drupalkit.jsonApi.getMenuItems("my_menu");
+
+  const res = result.unwrap();
+
+  t.snapshot(res);
+});
+
+test("Get menu items for non-existant menu", async (t) => {
+  const drupalkit = createDrupalkit();
+
+  server.use(
+    rest.get("*/jsonapi/menu_items/non_existant", (_req, res, ctx) =>
+      res(
+        ctx.set("Content-Type", "application/vnd.api+json"),
+        ctx.status(404),
+        ctx.json(JsonApiMenuItemsError),
+      ),
+    ),
+  );
+
+  const result = await drupalkit.jsonApi.getMenuItems("non_existant");
+
+  const err = result.expectErr("Expect error");
+
+  t.assert(err instanceof DrupalkitJsonApiError);
+  t.is(err.statusCode, 404);
+});
